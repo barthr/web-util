0743a0bedf6def2efae1816f9a928da8f23a9a29
Bart Fokker <bart@publicsonar.com>
2017-07-25 13:06:41 +0200

    some comments

--- chain.go
+++ chain.go
@@ -2,18 +2,23 @@ package web
 
 import "net/http"
 
+// MiddlewareFunc is the signature for a middleware function
 type MiddlewareFunc func(http.Handler) http.Handler
 
+// MiddlewareChain is the chain of handlers which are getting called LIFO
 type MiddlewareChain struct {
 	chain []MiddlewareFunc
 }
 
+// New returns a new MiddlewareChain with the given middleware functions
 func New(middlewareFuncs ...MiddlewareFunc) MiddlewareChain {
 	return MiddlewareChain{
 		chain: middlewareFuncs,
 	}
 }
 
+// Wrap takes a final handler and returns the handler through all
+// the middleware functions
 func (mwc MiddlewareChain) Wrap(final http.Handler) http.Handler {
 	if final == nil {
 		panic("cannot wrap nil handler")
@@ -25,10 +30,12 @@ func (mwc MiddlewareChain) Wrap(final http.Handler) http.Handler {
 	return final
 }
 
+// WrapHandlerFunc does the same as Wrap but takes an HandlerFunc instead of a http.Handler
 func (mwc MiddlewareChain) WrapHandlerFunc(final HandlerFunc) http.Handler {
 	return mwc.Wrap(final)
 }
 
+// Add create's a copy of the current chain and add's the given middlewareFuncs to the chain
 func (mwc MiddlewareChain) Add(middlewareFuncs ...MiddlewareFunc) MiddlewareChain {
 	newChain := mwc
 	copy(newChain.chain, mwc.chain)
